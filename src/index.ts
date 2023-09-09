1b4ea8e23cf1a7afb4e96aa8a9c151c5c6ecb63a
Harivansh-coder <harivanshthakur8@gmail.com>
2023-09-09 21:22:05 +0530

    done import alias and user validaiton schema :)

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
+import "module-alias/register";
 import express from "express";
 import mongoose from "mongoose";
-import { envVariables } from "./env";
+import { envVariables } from "@/env";
 
 // create a new express application instance
 const app: express.Application = express();
 
 // define a route handler for the default home page
-app.get("/", (req: express.Request, res: express.Response) => {
-  res.send("Hello " + req.query.name);
+app.get("/v1", (_req: express.Request, res: express.Response) => {
+  res.send("The sedulous hyena ate the antelope!");
 });
 
 // connect to MongoDB and start the express server
---
+++ src/schema/user.ts
@@ -0,0 +1,24 @@
+// user schema for request body validation
+
+import { z } from "zod";
+
+// zod schema for user signup
+export const userSignupSchema = z.object({
+  name: z
+    .string()
+    .trim()
+    .min(1, { message: "Name is required" })
+    .max(50, { message: "Name exceeds the maximum limit of characters" }),
+  email: z.string().trim().email({ message: "Invalid email" }),
+  password: z
+    .string()
+    .trim()
+    .min(8, { message: "Password must be at least 8 characters" })
+    .max(50, { message: "Password exceeds the maximum limit of characters" }),
+});
+
+// zod schema for user login
+export const userLoginSchema = z.object({
+  email: z.string().trim().email({ message: "Invalid email" }),
+  password: z.string().trim().min(8).max(50),
+});
